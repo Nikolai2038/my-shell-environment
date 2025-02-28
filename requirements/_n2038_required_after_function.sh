a8a27f117b8716dedf4970d8b8ddd625c4e1a0f1
Nikolai2038 <nikolai2038@yandex.ru>
2025-02-28 09:00:49 +0300

    Never fail "basename"
    See c63f1f69e9cf6ec3411e1e02132c05ca93067550.

--- requirements/_n2038_required_after_function.sh
+++ requirements/_n2038_required_after_function.sh
@@ -10,7 +10,7 @@ _n2038_required_after_function() {
   __n2038_function_name="$(eval "sed -En 's/^(function )?([a-z0-9_]+)[[:space:]]*\\(\\)[[:space:]]*\{[[:space:]]*\$/\\2/p' \"\${_N2038_PATH_TO_THIS_SCRIPT_${_N2038_PATH_TO_THIS_SCRIPT_NUMBER}}\"")" || return "$?"
   if [ -n "${__n2038_function_name}" ]; then
     # If this file is being executed - we execute function itself
-    if [ "$(basename "$0" 2> /dev/null)" = "$(eval "basename \"\${_N2038_PATH_TO_THIS_SCRIPT_${_N2038_PATH_TO_THIS_SCRIPT_NUMBER}}\"")" ]; then
+    if [ "$({ basename "$0" || true; } 2> /dev/null)" = "$(eval "basename \"\${_N2038_PATH_TO_THIS_SCRIPT_${_N2038_PATH_TO_THIS_SCRIPT_NUMBER}}\"")" ]; then
       "${__n2038_function_name}" "${@}" || exit "$?"
     fi
   fi
@@ -20,7 +20,7 @@ _n2038_required_after_function() {
 }
 
 # If this file is being executed
-if [ "$(basename "$0" 2> /dev/null)" = "_n2038_required_after_function.sh" ]; then
+if [ "$({ basename "$0" || true; } 2> /dev/null)" = "_n2038_required_after_function.sh" ]; then
   echo "This file is not meant to be executed directly - only sourced!" >&2
   exit 1
 # If this file is being sourced
