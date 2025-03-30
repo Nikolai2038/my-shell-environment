8c9ad0d6aa7919b0fb71d7848419c9a3855b8c52
Nikolai Ivanov <nikolai.ivanov.2038@gmail.com>
2025-03-31 00:44:49 +0300

    Add script to clear JetBrains data

---
+++ scripts/programs/jetbrains/n2038_jetbrains_clear_all_data.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+__N2038_PATH_TO_THIS_SCRIPT_FROM_ENVIRONMENT_ROOT="scripts/programs/jetbrains/n2038_jetbrains_clear_all_data.sh"
+
+# Required before imports
+# shellcheck disable=SC1091
+if [ "${_N2038_IS_MY_SHELL_ENVIRONMENT_INITIALIZED}" != "1" ]; then
+  # If we have not initialized the shell environment, but has it (for example, when starting "dash" from "bash"), then we will try to initialize it.
+  if [ -n "${_N2038_SHELL_ENVIRONMENT_PATH}" ]; then
+    . "${_N2038_SHELL_ENVIRONMENT_PATH}/n2038_my_shell_environment.sh" || _n2038_return "$?" || return "$?"
+  else
+    echo "\"my-bash-environment\" is not initialized. Please, initialize it first." >&2 && return 1 2> /dev/null || exit 1
+  fi
+fi
+_n2038_required_before_imports || { __n2038_return_code="$?" && [ "${__n2038_return_code}" = "${_N2038_RETURN_CODE_WHEN_FILE_IS_ALREADY_SOURCED}" ] && return "${_N2038_RETURN_CODE_WHEN_FILE_IS_ALREADY_SOURCED}" || _n2038_return "${__n2038_return_code}" || return "$?"; }
+
+# Imitate sourcing main file - to get correct references in IDE - it will not actually be sourced
+. "../../../n2038_my_shell_environment.sh" || _n2038_return "$?" || return "$?"
+
+# Imports
+. "./_constants.sh" || _n2038_return "$?" || return "$?"
+. "../../messages/_constants.sh" || _n2038_return "$?" || return "$?"
+. "../../messages/_n2038_print_error.sh" || _n2038_return "$?" || return "$?"
+. "../../messages/_n2038_print_info.sh" || _n2038_return "$?" || return "$?"
+. "../../messages/_n2038_print_list_items.sh" || _n2038_return "$?" || return "$?"
+. "../../messages/_n2038_print_success.sh" || _n2038_return "$?" || return "$?"
+. "./n2038_jetbrains_download.sh" || _n2038_return "$?" || return "$?"
+
+# Required after imports
+_n2038_required_after_imports || _n2038_return "$?" || return "$?"
+
+n2038_jetbrains_clear_all_data() {
+  rm -rf "${HOME}/.config/JetBrains" \
+    "${HOME}/.local/share/JetBrains" \
+    "${HOME}/.cache/JetBrains" \
+    "${HOME}/.java/.userPrefs/jetbrains" || { _n2038_unset "$?" && return "$?" || return "$?"; }
+  return 0
+}
+
+# Required after function
+_n2038_required_after_function "$@" || _n2038_return "$?" || return "$?"
